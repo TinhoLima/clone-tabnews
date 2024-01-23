2d7880fceccc375e629db262b8f52cb6ae9e3512
Alison Coutinho <93990888+TinhoLima@users.noreply.github.com>
2024-01-23 11:56:17 -0300

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Victora, eu amo você! Se você me ama, da uma risadinha! 😎</h1>
-}     
+  return <h1>Victora, eu amo você! Se você me ama, da uma risadinha! 😎</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
 
 export default Home;
